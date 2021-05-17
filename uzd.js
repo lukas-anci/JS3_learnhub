3ff058771c55e896cc8d3eeb4087fa6f821e499d
Lukas <lukas.ancikevicius@gmail.com>
2021-05-17 13:45:24 +0300

    pridejau

---
+++ uzd.js
@@ -0,0 +1,11 @@
+/**
+ * Git practice
+ *
+ * 1 sukurti index html faila, inicijuoti git
+ *
+ * 2 sukurti app.js ir style.css ir prideti visus failus kaip "init commit"
+ *
+ * 3. susikurti nauja branch html. ir dirbam tame branch
+ *
+ * 4. prijungti html saka prie pagrindines master sakos
+ */
